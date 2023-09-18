512219c370ebc7b4bf9603fc6ecf30774ea68f47
LUNA <liu.na@xbosoft.com>
2023-09-18 23:03:24 +0800

    HUD-1 页面的自动化

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from common_method import ApplyProcess
 
 personal_loan = ApplyProcess()
 
-# personal_loan.test()
+personal_loan.HUD()
 
-try:
-    personal_loan.access_application_interview()
-    personal_loan.generate_new_ssn()
-    personal_loan.search_new_ssn()
-    personal_loan.create_new_application()
-    personal_loan.checkout_application()
-    personal_loan.enter_identification_info()
-    personal_loan.payment_schedule()
-    personal_loan.setup_account()
-
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+# try:
+#     personal_loan.access_application_interview()
+#     personal_loan.generate_new_ssn()
+#     personal_loan.search_new_ssn()
+#     personal_loan.create_new_application()
+#     personal_loan.checkout_application()
+#     personal_loan.enter_identification_info()
+#     personal_loan.payment_schedule()
+#     personal_loan.setup_account()
+#
+# except Exception as e:
+#     print(f"An error occurred: {str(e)}")
 
 
