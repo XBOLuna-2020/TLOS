94206aa84f7eeff452e886cab082fa5efb81da5a
LUNA <liu.na@xbosoft.com>
2023-09-18 17:58:09 +0800

    创建了checkout RE loan的方法

--- common_method.py
+++ common_method.py
@@ -298,6 +298,65 @@ class ApplyProcess:
         time.sleep(2)
         print('checkout 成功')
 
+    def checkout_real_estate_loan(self):
+        # app创建成功后，进入checkout页面
+        # driver.get('http://mgrtest:tower1@uft-svr-080801/Tower080801/ApplicationCheckout/EditApplicationCheckout/36e8ce31-eb9c-48c3-ab14-b06d000d9a2e')
+        # 从首页进入check out页面
+        self.back_to_main_menu()
+        self.driver.find_element(By.PARTIAL_LINK_TEXT, 'Credit Application').click()
+        self.driver.find_element(By.PARTIAL_LINK_TEXT, 'Checkout Application').click()
+        # 输入app number 进入页面
+        self.driver.find_element(By.ID, 'ApplicationNumber').send_keys(app_number)
+        self.driver.find_element(By.ID, 'btnSearch').click()
+
+        # 输入Applicant Gross Monthly Salary
+        applicant_salary_monthly_salary = self.driver.find_element(By.ID,
+                                                              'ApplicantSalaryGarnishments_VerifiedMonthlySalary')
+        ActionChains(self.driver).double_click(applicant_salary_monthly_salary).perform()
+        applicant_salary_monthly_salary.send_keys(15000)
+
+        # Manager Approve info
+        self.driver.find_element(By.ID, 'LoanApprovals_0__Approved').send_keys('Y')
+        # 输入 Credit Limit
+        credit_limit = self.driver.find_element(By.ID, 'CreditLimit')
+        credit_limit.clear()  # 因为有默认值，需要先清除默认值，否则会在后面追加
+        credit_limit.send_keys(1000)
+        # 规定Amount Approved
+        amount_approved = self.driver.find_element(By.ID, 'LoanApprovals_0__ApprovedAmount')
+        amount_approved.clear()
+        amount_approved.send_keys(10000)
+
+        # 输入Plan A 的数据
+        plan_A_input_box = self.driver.find_element(By.ID, 'planA')
+        ActionChains(self.driver).double_click(plan_A_input_box).perform()  # Plan A 需要双击后才可以输入数字清除默认数值
+        plan_A_input_box.send_keys('1000')
+        plan_a = 'RN CUST, WANTS$1000, SELF EMPLOYED 3 YRS, BUYING HER HOME 2 YR RES, HAS C/S C 28% DTI, IM OK TO ADV $1000' \
+                 ' WILL BE A $4000 NL, DHPZ, 26 X 258 WILL NEED ID/POI/PP TO CLOSE VOIDED CHK AND 0 FORMER CL BC. '
+        self.driver.find_element(By.ID, 'commentText1').send_keys(plan_a)
+
+        # security info 选择为 Real Estate
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_SecurityOnLoan').send_keys('Real Estate')
+        # 弹窗单击OK
+        self.driver.switch_to.alert.accept()
+        # Property Address
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_RealEstateSecurity').send_keys('100 Mercantile St')
+        # Is Real Estate Insured (Y/N)
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_RealEstateInsured').send_keys('Y')
+        # Ins Expiration Date
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_RealEstateInsuranceExpiry').send_keys('1/1/2025')
+        # Index
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_Index').send_keys('39046')
+        # County/Parish
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_County').send_keys('Parish')
+        # 选择Will this loan pay off or refinance ANY existing mortgages? (Y/N)
+        self.driver.find_element(By.ID, 'PayoffRefiAnyMortgage').send_keys('Y')
+        # 选择Is loan a Homestead loan? (Y/N)
+        self.driver.find_element(By.ID, 'HomesteadLoan').send_keys('N')
+        # 单击底部create 按钮
+        self.driver.find_element(By.XPATH, '//*[@id="body"]/section/form[2]/p/input[3]').click()
+        time.sleep(2)
+        print('checkout 成功')
+
     def enter_identification_info(self):
         # 如果是check out 后，进入了Credit Application页面，则选择Enter_identification_info link
         self.driver.find_element(By.PARTIAL_LINK_TEXT, 'Enter/Edit Identification Information').click()
@@ -322,6 +381,8 @@ class ApplyProcess:
         self.driver.find_element(By.XPATH, '//*[@id="body"]/section/form/div/p[2]/input[3]').click()
         print('enter id infor 成功')
         time.sleep(2)
+
+
 
     def payment_schedule(self):
         # 从首页进入payment inquiry页面
@@ -382,50 +443,27 @@ class ApplyProcess:
         self.driver.quit()
 
     def test(self):
-        self.driver.get('http://mgrtest:tower1@uft-svr-020539/Tower020539/PaymentInquiry/FromApplication/19d5cab6-41ad-4812-99b5-b07c01738134')
-
-        # 输入loan_amount
-        ActionChains(self.driver).double_click(self.driver.find_element(By.ID, 'Input_RequestedAmount')).perform()
-        self.driver.find_element(By.ID, 'Input_RequestedAmount').send_keys(1000)
-        # 单击计算按钮
-        self.driver.find_element(By.ID, 'calcInquiryBtn').click()
+        self.driver.get('http://mgrtest:tower1@uft-svr-020539/Tower020539/ApplicationCheckout/EditApplicationCheckout/87b73a41-286f-49ce-9389-b0810026ce95')
+        # security info 选择为 Real Estate
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_SecurityOnLoan').send_keys('Real Estate')
+        # 弹窗单击OK
+        self.driver.switch_to.alert.accept()
+        # Property Address
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_RealEstateSecurity').send_keys('100 Mercantile St')
+        # Is Real Estate Insured (Y/N)
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_RealEstateInsured').send_keys('Y')
+        # Ins Expiration Date
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_RealEstateInsuranceExpiry').send_keys('1/1/2025')
+        # Index
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_Index').send_keys('39046')
+        # County/Parish
+        self.driver.find_element(By.ID, 'SecurityOnLoan1_County').send_keys('Parish')
+        # 选择Will this loan pay off or refinance ANY existing mortgages? (Y/N)
+        self.driver.find_element(By.ID, 'PayoffRefiAnyMortgage').send_keys('Y')
+        # 选择Is loan a Homestead loan? (Y/N)
+        self.driver.find_element(By.ID, 'HomesteadLoan').send_keys('N')
+        self.driver.find_element(By.XPATH, '//*[@id="body"]/section/form[2]/p/input[3]').click()
         time.sleep(2)
+        print('checkout 成功')
 
-        # 选择terms为18期
-        self.driver.find_element(By.ID, 'selectedLoanTerm').send_keys(21)
-        # 单击Confirm button
-        self.driver.find_element(By.XPATH, '//*[@id="confirmTerm"]/div[2]/a').click()
-
-        # 在pop-up上选择ok
-        prompt_object = self.driver.switch_to.alert
-        prompt_object.accept()
-        time.sleep(2)
-
-        # 单击print preview 按钮
-        self.driver.find_element(By.XPATH, '//*[@id="body"]/section/form/div[13]/div[2]/div/input[2]').click()
-        time.sleep(2)  # 切换到payment Inquiry页面
-        handles = self.driver.window_handles
-        self.driver.switch_to.window(handles[0])
-        # 单击eSign button
-        self.driver.find_element(By.XPATH, '//*[@id="body"]/section/form/div[13]/div[2]/div/input[3]').click()
-        time.sleep(3)
-        self.driver.switch_to.window(handles[0])
-
-        # 单击 Account Setup 按钮
-        self.driver.find_element(By.XPATH, '//*[@id="confirmTerm"]/div[2]/a[2]').click()
-        time.sleep(2)
-        # 单击 Disbursements of Proceeds 中的的下拉列表
-        self.driver.find_element(By.ID, 'Disbursements_Checks_0__IsPayableToCustomerYesNo').send_keys('Y')
-        # 找到，清除默认值，并输入Disbursements of Proceeds 的 Amount
-        disbursements_balance = self.driver.find_element(By.ID, 'TotalToAccountFor').get_attribute('value')
-        disbursements_checks_amount = self.driver.find_element(By.ID, 'Disbursements_Checks_0__LoanCheckAmount')
-        ActionChains(self.driver).double_click(disbursements_checks_amount).perform()
-        disbursements_checks_amount.send_keys(disbursements_balance)
-        # 单击eSign button,等待account号出现
-        self.driver.find_element(By.ID, 'openESig').click()
-        # 设置等待account出现后才打印出来
-        account = EC.text_to_be_present_in_element((By.ID, 'additionalHeaderInfo'), 'Account')
-        WebDriverWait(self.driver,10).until(account)
-        account_number = self.driver.find_element(By.XPATH, '//*[@id="additionalHeaderInfo"]/b[1]/a').text
-        print('account number:' + account_number)
 
--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from common_method import ApplyProcess
 
 personal_loan = ApplyProcess()
 
-# personal_loan.test()
 
-try:
-    personal_loan.access_application_interview()
-    personal_loan.generate_new_ssn()
-    personal_loan.search_new_ssn()
-    personal_loan.create_new_application()
-    personal_loan.checkout_application()
-    personal_loan.enter_identification_info()
-    personal_loan.payment_schedule()
-    personal_loan.setup_account()
+personal_loan.test()
 
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+# try:
+#     personal_loan.access_application_interview()
+#     personal_loan.generate_new_ssn()
+#     personal_loan.search_new_ssn()
+#     personal_loan.create_new_application()
+#     personal_loan.checkout_application()
+#     personal_loan.enter_identification_info()
+#     personal_loan.payment_schedule()
+#     # personal_loan.setup_account()
+#
+# except Exception as e:
+#     print(f"An error occurred: {str(e)}")
 
 
